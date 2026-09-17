5960a4510810b92bea35e01952e656b49c677b7a
vamsi123-paidi <vamsipaidi251002@gmail.com>
2026-09-17 12:03:51 +0530

    commited changes

--- expressjsbatch1/config/db.js
+++ expressjsbatch1/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const connectDB = async ()=>{
     try{
         mongoose.connect(process.env.MONGODB_URI)
-        console.log("mongodb connected sucessful")
+        console.log("mongodb connected sucessfulll")
     }catch{
         console.log("error while connecting to the mogdb atlas")
     }   
